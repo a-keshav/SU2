45fe8a2f28e19981b7f055be612b80354b2f7cbe
a-keshav <keshavanand1207@gmail.com>
2024-03-13 20:19:52 +0530

    undo changes

--- meson.py
+++ meson.py
@@ -22,7 +22,6 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 # Lesser General Public License for more details.
 #
-#
 # You should have received a copy of the GNU Lesser General Public
 # License along with SU2. If not, see <http://www.gnu.org/licenses/>.
 
